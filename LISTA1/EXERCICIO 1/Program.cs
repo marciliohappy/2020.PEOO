cd3af6f31134c0759cc47ee6eb8b32b628318c55
Marcílio Custódio da Silva <custodio.s@academico.ifrn.edu.br>
2020-02-21 15:51:35 -0300

    ALTERADO

---
+++ LISTA1/EXERCICIO 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICIO_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome: ");
+            string s = Console.ReadLine();
+            Console.WriteLine($"bem vindo ao c# {s}");
+            Console.ReadKey();
+        }
+    }
+}
